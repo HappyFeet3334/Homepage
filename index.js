487ac2baa43a97aff7a92888cc04e3343514970b
Tyler Burt <burttyl2@msu.edu>
2024-12-28 01:03:07 -0500

    Finished for now added shortcut buttons but there more to come :)

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ function displayTime() {
     let now = new Date();
     let hours = now.getHours();
     let minutes = now.getMinutes();
-
     let formattedTime = `${hours}:${minutes}`;
+
+    if(hours < 12 && minutes > 9){
+        formattedTime = `${hours}:${minutes}`;
+    }
+    else if(hours < 12 && minutes < 9){
+        formattedTime = `${hours}:0${minutes}`;
+    }
+    else if(hours > 12 && minutes > 9){
+        formattedTime = `${hours - 12}:${minutes}`;
+    }
+    else{
+        formattedTime = `${hours - 12}:0${minutes}`;
+    }
     document.getElementById("current-time").innerHTML = formattedTime;
   }
 
@@ -26,6 +38,37 @@ function displayTime() {
 
 //     let temp = today.
 //   }
+
+const githubIcon = document.getElementById('github');
+const youtubeIcon = document.getElementById('youtube');
+const spotifyIcon = document.getElementById('spotify');
+const redditIcon = document.getElementById('reddit');
+const discordIcon = document.getElementById('discord');
+
+githubIcon.addEventListener('click', function() {
+    // Action to perform when the icon is clicked
+    window.open('https://github.com', '_blank'); // Opens GitHub in a new tab
+});
+
+youtubeIcon.addEventListener('click', function() {
+    // Action to perform when the icon is clicked
+    window.open('https://www.youtube.com', '_blank'); // Opens GitHub in a new tab
+});
+
+spotifyIcon.addEventListener('click', function() {
+    // Action to perform when the icon is clicked
+    window.open('https://open.spotify.com', '_blank'); // Opens GitHub in a new tab
+});
+
+redditIcon.addEventListener('click', function() {
+    // Action to perform when the icon is clicked
+    window.open('https://www.reddit.com', '_blank'); // Opens GitHub in a new tab
+});
+
+discordIcon.addEventListener('click', function() {
+    // Action to perform when the icon is clicked
+    window.open('https://discord.com/channels/@me', '_blank'); // Opens GitHub in a new tab
+});
   
   window.onload = function(){
     displayDate();
